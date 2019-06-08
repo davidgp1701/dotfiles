f5e301f28be538f40aad442caeb9b892f38cef71
David García Pérez <davidgp@davidgp.com>
2019-06-08 13:03:03 +0200

    adding detection of operating system

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,6 +2,17 @@
 
 set -eo pipefail
 
+# Finding what the OS is:
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo "Working in a Linux environment"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Working in a Mac OS X environment"
+else
+  echo "OS not supported..."
+  exit 0 
+fi
+
 dirname=$(dirname "$0")
 
 # Preparing the vim configuration files
