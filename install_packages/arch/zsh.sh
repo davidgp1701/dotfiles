c2252a228b1bd68b0e1d699a0cd4af61b9617d86
David García Pérez <davidgp@gmail.com>
2019-10-19 18:11:42 +0200

    feat: Start from scratch of the repository
    Reseting how I want to keep everything in this dotfiles repository.
    
    For the moment there is the branch 'old_files' that keeps my old
    structure.

--- install_packages/arch/zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-# Install zsh and Oh my zsh!!!
-sudo pacman -S --noconfirm zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
