b41f6b79ee43f299ac78da85c6dc987c427dcd2a
David García Pérez <david.garcia@weareadaptive.com>
2021-01-16 17:17:53 +0100

    chore: Add installation of yay

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,21 @@
 # execute ansible-pull                         #
 ################################################
 
-set -eo pipefail
+set -euo pipefail
+IFS=$'\n\t'
 
-# For the momento only for Linux
+# For the moment only for Arch Linux
+
+# Update the system first
 sudo pacman --noconfirm -Syu
+
+# Install Ansible to manage dotfiles
 sudo pacman --noconfirm -S ansible git
 
+# Install yay to manage aur packages
+sudo pacman --noconfirm -S --needed base-devel
+
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+pushd /tmp/yay
+makepkg -si
+popd 
