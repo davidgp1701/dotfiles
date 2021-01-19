19d7512942519865de33ae8b23a5a5a2ac6d87e4
David García Pérez <david.garcia@weareadaptive.com>
2021-01-19 09:53:56 +0100

    fix: Go back to separate lua configs from vim

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
-" Lua configuration of neovim-lsp
-lua << EOF
 local nvim_lsp = require 'lspconfig'
 
 -- Terraform LSP configuraiton
 nvim_lsp.terraformls.setup{
   cmd = {'terraform-ls', 'serve'}
 }
-EOF
