71d4a4b2a1870f869f19dee80d06cf52dc71177c
David García Pérez <davidgp@davidgp.com>
2021-04-01 12:34:00 +0200

    chore: Add XDG env variables to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,31 @@ IFS=$'\n\t'
 # Update the system first
 # sudo pacman --noconfirm -Syu
 
+# Set the XDG configuration environment
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_CACHE_HOME="${HOME}/.cache"
+XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CONFIG_HOME
+export XDG_CACHE_HOME
+export XDG_DATA_HOME
+
+CARGO_HOME="$XDG_DATA_HOME/cargo"
+export $CARGO_HOME
+
+mkdir -p $XDG_CACHE_HOME
+mkdir -p $XDG_DATA_HOME
+
+# Enable this to bash
+echo 'XDG_CONFIG_HOME="${HOME}/.config"' >> "$HOME"/.bashrc
+echo 'XDG_CACHE_HOME="${HOME}/.cache"' >> "$HOME"/.bashrc
+echo 'XDG_DATA_HOME="${HOME}/.local/share"' >> "$HOME"/.bashrc
+echo 'export XDG_CONFIG_HOME' >> "$HOME"/.bashrc
+echo 'export XDG_CACHE_HOME' >> "$HOME"/.bashrc
+echo 'export XDG_DATA_HOME' >> "$HOME"/.bashrc
+echo '' >> "$HOME"/.bashrc
+echo 'CARGO_HOME="$XDG_DATA_HOME/cargo"' >> "$HOME"/.bashrc
+echo 'export CARGO_HOME' >> "$HOME"/.bashrc
+
 # Install Ansible to manage dotfiles
 if ! sudo pacman -Qi ansible
 then
