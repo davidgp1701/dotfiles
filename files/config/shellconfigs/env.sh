ded69b487a7c418f4477b04a35e249d2cc5a940a
David García Pérez <davidgp@davidgp.com>
2021-04-07 17:29:28 +0200

    chore: Externalize Okta configuration

--- files/config/shellconfigs/env.sh
+++ files/config/shellconfigs/env.sh
@@ -1,8 +1,4 @@
 export PATH="${GOPATH}/bin:${PATH}"
 
 # # Add local home to the path
-# PATH="${PATH}:/home/david/.local/bin"
-
-# # Add Okta scripts to the path
-# PATH="${PATH}:/home/david/.okta/bin"
-
+PATH="${PATH}:/home/david/.local/bin"
