585f05bb64809e5bec15ceee09af6d661a178f10
David García Pérez <davidgp@davidgp.com>
2021-05-09 09:52:51 +0200

    feat: Add rtorrent configuration

---
+++ files/local/bin/magnet2torrent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+watch_folder="${HOME}/Downloads/torrents/watch"
+
+pushd "$watch_folder" || exit
+[[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit;
+aria2c --bt-metadata-only=true --bt-save-metadata=true "$1"
+
