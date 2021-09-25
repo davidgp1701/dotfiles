430e9ca33af1914cb388cf1ce7244998ae20d6bf
David Garcia Perez <davidgp@gmail.com>
2021-09-25 17:16:11 +0200

    chore: Remove no used modules in tree-sitter

--- files/config/nvim/lua/cfg_treesitter/init.lua
+++ files/config/nvim/lua/cfg_treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = { "elixir", "godotResource", "ocamllex", "teal" },
+  ignore_install = { "devicetree", "elixir", "gdscript", "godotResource", "ocamllex", "teal" },
   highlight = {
     enable = true,          -- false will disable the whole extension
   },
