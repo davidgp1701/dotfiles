a6222093c760cfd1082e98a379188cb6f906d792
David Garcia Perez <david.garcia@weareadaptive.com>
2021-09-30 11:30:05 +0200

    chore: Some more work tools changes

--- files/config/shellconfigs/okta.sh
+++ files/config/shellconfigs/okta.sh
@@ -1,8 +1,16 @@
- # Okta
- #OktaAWSCLI
- if [[ -f "$HOME/.okta/bash_functions" ]]; then
-     . "$HOME/.okta/bash_functions"
- fi
- if [[ -d "$HOME/.okta/bin" && ":$PATH:" != *":$HOME/.okta/bin:"* ]]; then
-     PATH="$HOME/.okta/bin:$PATH"
+# Okta
+#OktaAWSCLI
+if [[ -f "$HOME/.okta/bash_functions" ]]; then
+   . "$HOME/.okta/bash_functions"
+fi
+if [[ -d "$HOME/.okta/bin" && ":$PATH:" != *":$HOME/.okta/bin:"* ]]; then
+   PATH="$HOME/.okta/bin:$PATH"
+fi
+
+## New configuration place
+if [[ -f "$HOME/.local/share/okta/bash_functions" ]]; then
+    . "$HOME/.local/share/okta/bash_functions"
+fi
+if [[ -d "$HOME/.local/share/okta/bin" && ":$PATH:" != *":$HOME/.local/share/okta/bin:"* ]]; then
+    PATH="$HOME/.local/share/okta/bin:$PATH"
 fi
