a039343ceabf41324e7a75aa6894d6da8ccc1b40
David Garcia Perez <david.garcia@weareadaptive.com>
2021-10-03 17:58:18 +0200

    feat: Add Winbox installation

--- files/config/shellconfigs/alias.sh
+++ files/config/shellconfigs/alias.sh
@@ -10,3 +10,6 @@ alias v="nvim"
 
 # Force programs to follow the XDG dir structure
 alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
+
+# Winbox wants to use Xterm as terminal
+alias winbox="TERM=xterm /snap/bin/winbox"
