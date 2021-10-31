305df2cbd99ad56f81025c90fdcd473959e61dc9
David Garcia Perez <david.garcia@weareadaptive.com>
2021-10-31 11:16:01 +0100

    chore: Update nvim-tree config for latests version

--- files/config/nvim/lua/cfg_nvim-tree/init.lua
+++ files/config/nvim/lua/cfg_nvim-tree/init.lua
@@ -48,11 +48,14 @@ require'nvim-tree'.setup {
       -- list of mappings to set on the tree manually
       list = {}
     }
+  },
+
+  filters = {
+    dotfiles = false
   }
 }
 
 -- nvim-tree file namager configuration
-vim.g.nvim_tree_ignore = {'.git', '.terraform', 'node_modules', '.cache'}
 vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
 vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
@@ -61,7 +64,6 @@ vim.g.nvim_tree_width = 40 -- 30 by default, can be width_in_columns or 'width_i
 vim.g.nvim_tree_side = 'left' -- left by default
 vim.g.nvim_tree_gitignore = 1 -- 0 by default
 -- vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' } -- empty by default, don't auto open tree on specific filetypes.
-vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
 -- vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
