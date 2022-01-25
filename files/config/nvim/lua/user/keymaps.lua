ee2849330ff582f702dc9153830ec74820c5fd1e
David Garcia Perez <david.garcia@weareadaptive.com>
2022-01-25 15:59:48 +0100

    chore: neovim add git finder

--- files/config/nvim/lua/user/keymaps.lua
+++ files/config/nvim/lua/user/keymaps.lua
@@ -40,6 +40,7 @@ keymap('n', '<C-P>', ':Telescope neoclip a extra=plus<CR>', opts)
 keymap('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', opts)
 keymap('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', opts)
 keymap('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', opts)
+keymap('n', '<leader>fi', '<cmd>lua require(\'telescope.builtin\').git_status()<cr>', opts)
 
 -- VISUAL --
 -- Better indentation
