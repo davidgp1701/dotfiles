b93c4faf1a69ee666d107487c92a4bbb83f4f31f
David Garcia Perez <david.garcia@weareadaptive.com>
2022-09-23 11:22:18 +0200

    feat: Upgrade ssm script

--- files/local/bin/ssm.sh
+++ files/local/bin/ssm.sh
@@ -9,6 +9,13 @@ else
   profile="$AWS_PROFILE"
 fi
 
+if [ -z ${AWS_REGION+x} ]
+then
+  region="eu-west-1"
+else
+  region="$AWS_REGION"
+fi
+
 instance_id="$(\
     aws ec2 describe-instances \
         --profile "${profile}" \
@@ -16,10 +23,10 @@ instance_id="$(\
             "Name=tag:Name,Values=*${instance_name}" \
             "Name=instance-state-name,Values=running" \
         --query "Reservations[0].Instances[0].InstanceId" \
-        --region "eu-west-1" \
+        --region "${region}" \
         --output text \
 )"
 
 if [[ $? -eq 0 ]] && [[ -n "${instance_id}" ]] && [[ "${instance_id}" != "None" ]] ; then
-    aws ssm start-session --profile "${profile}" --target "${instance_id}" --region "eu-west-1"
+    aws ssm start-session --profile "${profile}" --target "${instance_id}" --region "${region}"
 fi
