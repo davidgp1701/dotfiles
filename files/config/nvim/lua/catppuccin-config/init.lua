e1645abe99e6f6ee2cd3f2bb499db0daa9d54191
David Garcia Perez <david.garcia@weareadaptive.com>
2022-09-26 17:00:18 +0200

    chore: Update catppuccin-config

--- files/config/nvim/lua/catppuccin-config/init.lua
+++ files/config/nvim/lua/catppuccin-config/init.lua
@@ -88,5 +88,5 @@ catppuccin.setup({
   }
 })
 
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 vim.cmd [[colorscheme catppuccin]]
