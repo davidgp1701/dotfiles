67397a14e347faff27ffaa84c7fca83358c68f43
David Garcia Perez <david.garcia@weareadaptive.com>
2022-10-03 11:47:59 +0200

    fix: Update start_hyprland script

--- files/local/bin/start_hyprland.sh
+++ files/local/bin/start_hyprland.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 cd ~
 
 export SDL_VIDEODRIVER=wayland
-export MOZ_ENABLE_WAYLAND=1
+# export MOZ_ENABLE_WAYLAND=1
 export XKB_DEFAULT_OPTIONS=caps:escape
 export _JAVA_AWT_WM_NONREPARENTING=1
 export XCURSOR_SIZE=24
