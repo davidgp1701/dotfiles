270b8b7335f623f4231f3b263a6d7d016c9b794e
David Garcia Perez <david.garcia@weareadaptive.com>
2023-05-02 10:36:35 +0200

    chore: change to catppuccin again

--- files/config/nvim/after/plugin/colors.lua
+++ files/config/nvim/after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function MyColors(color)
-	color = color or "rose-pine"
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 end
 
--- files/config/nvim/lua/david/packer.lua
+++ files/config/nvim/lua/david/packer.lua
@@ -12,13 +12,15 @@ return require('packer').startup(function(use)
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
-  })
+  -- use({
+  --   'rose-pine/neovim',
+  --   as = 'rose-pine',
+  --   config = function()
+  --     vim.cmd('colorscheme rose-pine')
+  --   end
+  -- })
+
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   -- use({
   --   'EdenEast/nightfox.nvim',
