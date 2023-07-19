105878bf49c2f793437568fdaa3bf9493ca38d8c
David Garcia Perez <david.garcia@weareadaptive.com>
2023-07-19 10:02:18 +0200

    chore(nvim): switch to LazyVim

--- files/config/nvim/after/plugin/indent-blankline.lua
+++
@@ -1,4 +0,0 @@
-vim.g.indent_blankline_buftype_exclude = {'terminal'}
-vim.g.indent_blankline_filetype_exclude = {'help', 'startify', 'dashboard', 'packer', }
-vim.g.indent_blankline_char = '▏'
-vim.g.indent_blankline_use_treesitter=true
