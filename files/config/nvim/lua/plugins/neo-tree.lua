158a3f6d5d797a8b38faa21448485fd16dccd643
David Garcia Perez <david.garcia@weareadaptive.com>
2023-07-20 08:27:50 +0200

    chore(nvim): update lazyvim configuration for my needs

---
+++ files/config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
+}
