0a5a857ee3c60314557e2be01cc33e51904ebbc7
David Garcia Perez <david.garcia@weareadaptive.com>
2023-08-30 12:45:29 +0200

    fix(nvim): lsp root folder

--- files/config/nvim/lua/config/lazy.lua
+++ files/config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
