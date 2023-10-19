34ab45421bbd884365da763c276e534c99fd497d
David Garcia Perez <david.garcia@weareadaptive.com>
2023-10-19 11:54:11 +0200

    chore(nvim): adjust indent blankline colors

--- files/config/nvim/lua/davidgp/plugins/nvim-indent-blankline.lua
+++ files/config/nvim/lua/davidgp/plugins/nvim-indent-blankline.lua
@@ -6,29 +6,29 @@ return {
     opts = {},
     config = function()
       local highlight = {
-          "RainbowRed",
-          "RainbowYellow",
-          "RainbowBlue",
-          "RainbowOrange",
-          "RainbowGreen",
-          "RainbowViolet",
-          "RainbowCyan",
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
       }
-      local hooks = require "ibl.hooks"
+      local hooks = require("ibl.hooks")
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-          vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-          vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-          vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-          vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-          vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-          vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-          vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#f38ba8" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#f9e2af" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#89b4fa" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#fab387" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#a6e3a1" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#cba6f7" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#89dceb" })
       end)
 
       vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup { scope = { highlight = highlight } }
+      require("ibl").setup({ scope = { highlight = highlight } })
 
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
