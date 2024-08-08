84f1cd47246ddd11196230e7bdab46924831c1fc
David Garcia Perez <david.garcia@weareadaptive.com>
2024-08-08 12:28:14 +0200

    chore: lets go back to gruvbox

---
+++ files/config/nvim/lua/davidgp/plugins/gruvbox.lua
@@ -0,0 +1,9 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000 ,
+  config = function()
+    require("gruvbox").setup()
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
+  end,
+}
