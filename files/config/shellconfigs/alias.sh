e3ca9a5af275fbd651329543ac8e72b693058f47
David Garcia Perez <david.garcia@weareadaptive.com>
2024-09-02 08:42:29 +0200

    chore: still using the old name for WinBox

--- files/config/shellconfigs/alias.sh
+++ files/config/shellconfigs/alias.sh
@@ -13,3 +13,6 @@ alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 
 # Only for Kitty, dissable if using other TERM
 alias ssh='TERM="xterm-256color" ssh'
+
+# Alias for Winbox
+alias winbox="WinBox"
