52197e8293e07412ae3111afefdf905ef35f7cd8
David Garcia Perez <david.garcia@weareadaptive.com>
2024-10-01 17:18:41 +0200

    chore: add jump posibilities

--- user/app/nvim/plugins/cmp.nix
+++ user/app/nvim/plugins/cmp.nix
@@ -13,9 +13,39 @@
           "<C-n>" = "cmp.mapping.select_next_item()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
           "<C-e>" = "cmp.mapping.abort()";
           "<CR>" = "cmp.mapping.confirm({ select = false })";
+          "<C-Space>" = ''
+            cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            }
+          '';
+          "<Tab>" = ''
+             cmp.mapping(function(fallback)
+               local status_ok, luasnip = pcall(require, "luasnip")
+
+               if cmp.visible() then
+                 cmp.select_next_item()
+               elseif status_ok and luasnip.locally_jumpable(1) then
+                 luasnip.jump(1)
+               else
+                 fallback()
+               end
+             end, { "i", "s" })
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(function(fallback)
+              local status_ok, luasnip = pcall(require, "luasnip")
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif status_ok and luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
         };
 
         sources = [
